09497361a5c6b7e57fa07d45fbd1314133727864
Veli-Pekka Parkkinen <parkkinenv@gmail.com>
2022-09-22 08:43:47 +0200

    test all cors

--- cor_test_script.R
+++ cor_test_script.R
@@ -12,25 +12,55 @@ source(here("functions.R"))
 # could also try ideal data with one irrelevant factor added first.
 # any case, 
 # change n = 4 to some reasonable number (100? 1000? 10000?) 
-dats <- replicate(n = 4, 
-                  noisyDat(n.asf = 1, noisefraction = .15), 
+dats <- replicate(n = 1000, 
+                  noisyDat(set_N = 100, n.asf = 1, noisefraction = .1), 
                   simplify = FALSE)
 
 # store the DGSs for each data set
 targets <- lapply(dats, function(x) attributes(x)$target)
+targets <- lapply(targets, function(x) gsub("^\\(|\\)$", "", x))
+targets <- lapply(targets, cna::noblanks)
+outcomes <- lapply(targets, rhs)
 
 # add irrelevant factor to datasets
 datsX <- lapply(dats, function(x) {x$X <- rbinom(nrow(x), 1, .5); return(x)})
 
 # add irrelevant factor to the DGS models
-modelsX <- lapply(targets, add_factor)
+# modelsX <- lapply(targets, add_factor)
 
-# datasets with irrelevant factor X are now in a list datsX. Corresponding
-# models with one irrelevant factor value X or x added to the DGS 
-# are in a list modelsX.
-# The test loop should go through these such
-# that for index i we do 
-# cna_cortest(model = modelsX[[i]], data = datsX[[i]], ...), and 
-# then calculate whatever we want to calculate from the results.
+cor_matrices <- lapply(datsX, cor)
+cor_matrices_outcome <- vector("list", length(cor_matrices))
 
+for(i in seq_along(cor_matrices)){
+  cor_matrices_outcome[[i]] <- cor_matrices[[i]][
+    ,which(dimnames(cor_matrices[[i]])[[2]] == outcomes[[i]])
+    ]
+  cor_matrices_outcome[[i]] <- cor_matrices_outcome[[i]][
+    -which(names(cor_matrices_outcome[[i]]) == outcomes[[i]])
+    ]
+}
+
+cor_matrices_outcome <- lapply(cor_matrices_outcome, abs)
+
+cor_matrices_outcome_nox <- lapply(cor_matrices_outcome, 
+                                   function(x)
+                                     x[-which(names(x) == "X")])
+
+nox_grand_mean <- mean(unlist(cor_matrices_outcome_nox))
+
+x_cors <- lapply(cor_matrices_outcome,
+                 function(x) x[which(names(x) == "X")])
+
+x_mean <- mean(unlist(x_cors))
+
+# cor_matrices_differences <- lapply(cor_matrices_outcome, 
+#                                    function(x) 
+#                                      x[
+#                                        -which(names(x) == "X")
+#                                        ] - x[which(names(x) == "X")])
+# 
+# mean_diffs <- lapply(cor_matrices_differences, mean)
+# grand_mean_diff <- mean(unlist(mean_diffs))
+
+x_mean / nox_grand_mean
 
